f2ac2cfdd506ee06096c84daf1f1ac5c6dda709a
David Jorm <david.jorm@gmail.com>
2014-12-05 15:16:29 +1000

    Dozer RCE research

---
+++ dozer-rce/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cp dozerBeanMapping.xml /tmp/
+mvn clean compile assembly:single && java -jar target/dozersploit-1.0-SNAPSHOT-jar-with-dependencies.jar
+rm -f /tmp/dozerBeanMapping.xml
+mvn clean
