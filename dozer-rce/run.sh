40a32f7e24595f6a2e9382d572a9c6151de9556d
David Jorm <david.jorm@gmail.com>
2014-12-14 23:16:17 +1000

    RCE now working if vuln spring is on the server classpath

--- dozer-rce/run.sh
+++ dozer-rce/run.sh
@@ -2,4 +2,5 @@
 cp dozerBeanMapping.xml /tmp/
 mvn clean compile assembly:single && java -jar target/dozersploit-1.0-SNAPSHOT-jar-with-dependencies.jar
 rm -f /tmp/dozerBeanMapping.xml
+rm -rf proxy.ser
 mvn clean
